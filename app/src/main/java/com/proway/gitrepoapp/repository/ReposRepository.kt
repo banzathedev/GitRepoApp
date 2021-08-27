30b8c86600f53ca5a326422321844f96402e481d
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-27 14:23:35 -0300

    Repositoru Creation

--- app/src/main/java/com/proway/gitrepoapp/repository/ReposRepository.kt
+++ app/src/main/java/com/proway/gitrepoapp/repository/ReposRepository.kt
@@ -1,4 +1,13 @@
 package com.proway.gitrepoapp.repository
 
+
+import com.proway.gitrepoapp.BuildConfig
+import com.proway.gitrepoapp.services.RetrofitBuilder
+
 class ReposRepository {
+    fun getAllRepos(){
+        RetrofitBuilder.getInstance(BuildConfig.GITHUB_API_URL)
+
+
+    }
 }
